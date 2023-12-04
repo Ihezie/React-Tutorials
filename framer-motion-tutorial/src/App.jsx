a8afe7e5e9f0ea7fa1790ba95cf1075f87ee0afd
Ogbuehi Raymond <raymond@Ogbuehis-MacBook-Air.local>
2023-12-04 16:21:25 +0100

    created react tutorials repository

---
+++ framer-motion-tutorial/src/App.jsx
@@ -0,0 +1,12 @@
+import BasicsOfMotion from "./components/BasicsOfMotion";
+import Gestures from "./components/Gestures";
+
+function App() {
+  return (
+    <div>
+      {/* <BasicsOfMotion /> */}
+      <Gestures/>
+    </div>
+  );
+}
+export default App;
---
+++ framer-motion-tutorial/src/components/Gestures.jsx
@@ -0,0 +1,50 @@
+import { motion, MotionConfig } from "framer-motion";
+const Gestures = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}
+    >
+      <MotionConfig
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+        }}
+      >
+        <motion.button
+          whileHover={{
+            scale: 1.05,
+          }}
+          whileTap={{
+            scale: 0.95,
+            rotate: "2.5deg",
+          }}
+          className="example-button"
+        >
+          Click me!
+        </motion.button>
+        <motion.button
+          style={{
+            backgroundColor: "red",
+          }}
+          whileHover={{
+            scale: 1.05,
+          }}
+          whileTap={{
+            scale: 0.95,
+            rotate: "-2.5deg",
+          }}
+          className="example-button"
+        >
+          Click me!
+        </motion.button>
+      </MotionConfig>
+    </div>
+  );
+};
+
+export default Gestures;
